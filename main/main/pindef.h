158558a427d169dde0af8dfc6fd0a71d5224d7f9
bpatern <99623637+bpatern@users.noreply.github.com>
2026-03-19 23:45:49 -0400

    Delete main/main directory

--- main/main/pindef.h
+++
@@ -1,8 +0,0 @@
-#define pin_dirSwitch 39
-#define pin_motorRunSwitch 38
-#define pin_lampSwitch 37
-
-#define relay_dirPin1 GPIO_NUM_2
-#define relay_dirPin2 GPIO_NUM_4
-#define relay_motor GPIO_NUM_5
-#define relay_lamp GPIO_NUM_18
